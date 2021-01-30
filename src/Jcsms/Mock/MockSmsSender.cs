4da79321acd84b7a67789d0d5d0a141ae790fa0d
吴宏伟 <wlclass@163.com>
2021-01-30 14:32:49 +0800

    添加了模拟短信服务

---
+++ src/Jcsms/Mock/MockSmsSender.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jcsms.Mock
+{
+    public class MockSmsSender : ISmsSender
+    {
+        public string Provider => "模拟短信服务";
+
+        public ResultBase<QueryBalanceResult> GetBalance()
+        {
+            return new ResultBase<QueryBalanceResult>()
+            {
+                Code = 0,
+                Message = "OK",
+                Succeed = true,
+                Data = new QueryBalanceResult()
+                {
+                    Balance = 9999999,
+                    Time = DateTime.Now
+                },
+            };
+        }
+
+        public SmsSentResult Send(IEnumerable<string> phoneNumbers, string templateCode, object smsData)
+        {
+
+            return new SmsSentResult()
+            {
+                Succeed = true
+            };
+
+        }
+
+        public SmsSentResult SendCode(string phoneNumber, string code, string scope, object smsData)
+        {
+            return new SmsSentResult()
+            {
+                Succeed = true
+            };
+        }
+
+        public SmsSentResult SendCode(string phoneNumber, string code, string scope, string content, object smsData)
+        {
+            return new SmsSentResult()
+            {
+                Succeed = true
+            };
+        }
+
+        public SmsSentResult SendFreeMessage(IEnumerable<string> phoneNumbers, string message)
+        {
+            return new SmsSentResult()
+            {
+                Succeed = true
+            };
+        }
+    }
+}
